9a0df6d7a9b1a04084f269e3626f9643ddaaaf6c
Taloth Saldono <Taloth@users.noreply.github.com>
2018-12-24 19:21:45 +0100

    Tweaked GZipPipeline.

--- src/Sonarr.Http/Extensions/Pipelines/GZipPipeline.cs
+++ src/Sonarr.Http/Extensions/Pipelines/GZipPipeline.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.IO.Compression;
 using System.Linq;
@@ -25,6 +25,131 @@ namespace Sonarr.Http.Extensions.Pipelines
             pipelines.AfterRequest.AddItemToEndOfPipeline(CompressResponse);
         }
 
+        private class ExceptionSafeGZipStream : GZipStream
+        {
+            private class ExceptionCatchingStream : Stream
+            {
+                private Stream _innerStream;
+                public Exception Exception;
+
+                public ExceptionCatchingStream(Stream stream)
+                {
+                    _innerStream = stream;
+                }
+                public override bool CanRead => _innerStream.CanRead;
+                public override bool CanSeek => _innerStream.CanSeek;
+                public override bool CanWrite => _innerStream.CanWrite;
+                public override long Length => _innerStream.Length;
+
+                public override long Position { get => _innerStream.Position; set => _innerStream.Position = value; }
+
+                public override void Flush()
+                {
+                    _innerStream.Flush();
+                }
+
+                public override int Read(byte[] buffer, int offset, int count)
+                {
+                    return _innerStream.Read(buffer, offset, count);
+                }
+
+                public override long Seek(long offset, SeekOrigin origin)
+                {
+                    return _innerStream.Seek(offset, origin);
+                }
+
+                public override void SetLength(long value)
+                {
+                    _innerStream.SetLength(value);
+                }
+
+                public override void Write(byte[] buffer, int offset, int count)
+                {
+                    try
+                    {
+                        _innerStream.Write(buffer, offset, count);
+                    }
+                    catch (Exception ex)
+                    {
+                        if (Exception != null)
+                            Exception = ex;
+                    }
+                }
+            }
+
+            private Stream _innerStream;
+            private ExceptionCatchingStream _catcher;
+
+            private Stream WrapInner(Stream innerStream)
+            {
+                _innerStream = innerStream;
+                _catcher = new ExceptionCatchingStream(innerStream);
+                return _catcher;
+            }
+
+            public ExceptionSafeGZipStream(Stream stream, CompressionMode mode)
+                : base(new ExceptionCatchingStream(stream), CompressionMode.Compress, true)
+            {
+                _innerStream = stream;
+                _catcher = base.BaseStream as ExceptionCatchingStream;
+            }
+
+            public override void Write(byte[] array, int offset, int count)
+            {
+                base.Write(array, offset, count);
+
+                if (_catcher.Exception != null)
+                {
+                    var ex = _catcher.Exception;
+                    _catcher.Exception = null;
+                    throw ex;
+                }
+            }
+        }
+
+        private class ThrowingStream : Stream
+        {
+            private long _position;
+
+            public override bool CanRead => false;
+
+            public override bool CanSeek => false;
+
+            public override bool CanWrite => true;
+
+            public override long Length => _position;
+
+            public override long Position { get => _position; set => throw new NotSupportedException(); }
+
+            public override void Flush()
+            {
+
+            }
+
+            public override int Read(byte[] buffer, int offset, int count)
+            {
+                throw new NotSupportedException();
+            }
+
+            public override long Seek(long offset, SeekOrigin origin)
+            {
+                throw new NotSupportedException();
+            }
+
+            public override void SetLength(long value)
+            {
+                throw new NotSupportedException();
+            }
+
+            public override void Write(byte[] buffer, int offset, int count)
+            {
+                if (_position + count > 10000)
+                    throw new IOException("Some Error");
+
+                _position += count;
+            }
+        }
+
         private void CompressResponse(NancyContext context)
         {
             var request = context.Request;
@@ -45,7 +170,7 @@ namespace Sonarr.Http.Extensions.Pipelines
                     response.Headers["Content-Encoding"] = "gzip";
                     response.Contents = responseStream =>
                     {
-                        using (var gzip = new GZipStream(responseStream, CompressionMode.Compress, true))
+                        using (var gzip = new ExceptionSafeGZipStream(responseStream, CompressionMode.Compress))
                         using (var buffered = new BufferedStream(gzip, 8192))
                         {
                             contents.Invoke(buffered);
